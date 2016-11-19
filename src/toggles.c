a7ae992a856c0691b436131c3f6cfe97b0f8b350
AmatCoder <amatcoder@gmail.com>
2016-11-19 19:43:50 +0000

    Prepare to 0.8.3 release

--- src/toggles.c
+++ src/toggles.c
@@ -357,7 +357,7 @@ gchar* get_cfg(const gchar *home, guidata *gui)
   #endif
 
   if (g_file_test (cfg_path, G_FILE_TEST_IS_REGULAR))
-    print_log("Starting Mednaffe 0.8.2\n\
+    print_log("Starting Mednaffe 0.8.3\n\
 Mednafen 09x configuration file found.\n", FE, gui);
   else return NULL;
 
