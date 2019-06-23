7a020f49993792c23a2af1d0762dfa3c4dba4e40
AmatCoder <amatcoder@gmail.com>
2019-06-23 13:37:53 +0200

    Upload forgotten files (issue #97)

---
+++ src/widgets/joystick.h
@@ -0,0 +1,52 @@
+/*
+ * joystick.h
+ * 
+ * Copyright 2013-2019 AmatCoder
+ * 
+ * This file is part of Mednaffe.
+ * 
+ * Mednaffe is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Mednaffe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Mednaffe; if not, see <http://www.gnu.org/licenses/>.
+ *
+ *  
+ */
+
+
+#ifndef __JOYSTICK_H__
+#define __JOYSTICK_H__
+
+#include <glib.h>
+
+
+G_BEGIN_DECLS
+
+
+typedef struct{
+  gint num;
+  gint type;
+  gchar *name;
+  gchar *id;
+  gpointer data1;
+}joy_s;
+
+
+GSList* init_joys ();
+void discard_read (GSList *list);
+gchar* read_joys (GSList *list);
+void close_joys (GSList *list);
+gchar* value_to_text (GSList* listjoy, const gchar* value);
+
+
+G_END_DECLS
+
+#endif
---
+++ src/widgets/joystick_dummy.c
@@ -0,0 +1,58 @@
+/*
+ * joystick_dummy.c
+ * 
+ * Copyright 2013-2019 AmatCoder
+ * 
+ * This file is part of Mednaffe.
+ * 
+ * Mednaffe is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Mednaffe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Mednaffe; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * 
+ */
+
+
+#include "joystick.h"
+
+
+void
+close_joys (GSList *list)
+{
+}
+
+
+gchar*
+value_to_text (GSList* listjoy, const gchar* value)
+{
+  return g_strdup (value);
+}
+
+
+GSList*
+init_joys ()
+{
+  return NULL;
+}
+
+
+void
+discard_read (GSList *list)
+{
+}
+
+
+gchar*
+read_joys (GSList *list)
+{
+  return NULL;
+}
