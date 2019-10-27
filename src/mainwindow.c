AmatCoder/mednaffe

commit 6543f1370485e019623b5a29706fcd13b5024f6d

Optimize perfomance when widgets are updated
- Now they are updated only when they are mapped, not all at once.
- UPDATED_PROPERTY was removed. Now it is a public method.

src/mainwindow.c
@@ -53,6 +53,7 @@ struct _MainWindowClass {
 
 struct _MainWindowPrivate {
   GSList* list;
+  GSList* map_list;
   MedProcess* med_process;
   AboutWindow* about_window;
   BiosWindow* bios_window;
@@ -224,8 +225,7 @@ main_window_paned_list_item_selected (PanedList* sender,
 
 
 static void
-main_window_medwid_set_sensitive (MainWindow* self,
-                                  gboolean b)
+main_window_medwid_update_all (MainWindow* self)
 {
   g_return_if_fail (self != NULL);
 
@@ -235,7 +235,9 @@ main_window_medwid_set_sensitive (MainWindow* self,
 
   for(it = priv->list; it != NULL; it = it->next)
   {
-    gtk_widget_set_sensitive ((GtkWidget*) it->data, b);
+    med_widget_set_modified ((MedWidget*) it->data, FALSE);
+    med_widget_set_updated ((MedWidget*) it->data, FALSE);
+    gtk_widget_set_sensitive ((GtkWidget*) it->data, FALSE);
   }
 }
 
@@ -256,7 +258,7 @@ main_window_build_command (MainWindow* self, const gchar* game_path)
 
   for(it = priv->list; it != NULL; it = it->next)
   {
-    if (med_widget_get_updated (it->data))
+    if (med_widget_get_modified (it->data))
     {
       command = g_renew(const gchar*, command, i+5);
 
@@ -298,7 +300,6 @@ main_window_paned_list_launched (PanedList* _sender,
   {
     med_list_joy_enable_all(priv->listjoy, FALSE);
     gtk_widget_set_sensitive (((GtkWidget*) priv->launch_button), FALSE);
-    main_window_medwid_set_sensitive (mw, FALSE);
 
     const gchar *action = med_widget_get_value ((MedWidget*) priv->preferences->action_launch);
 
@@ -330,6 +331,8 @@ main_window_paned_list_launched (PanedList* _sender,
 #ifdef G_OS_WIN32
     g_free (selection2);
 #endif
+
+    main_window_medwid_update_all (mw);
   }
 }
 
@@ -457,7 +460,7 @@ main_window_set_values (MainWindow* self)
   g_return_if_fail (self != NULL);
   MainWindowPrivate* priv = main_window_get_instance_private (self);
 
-  for(it = priv->list; it != NULL; it = it->next)
+  for(it = priv->map_list; it != NULL; it = it->next)
   {
     const gchar* command;
     const gchar* tmp = NULL;
@@ -473,7 +476,8 @@ main_window_set_values (MainWindow* self)
     if (tmp != NULL)
     {
       med_widget_set_value (it->data, tmp);
-      med_widget_set_updated (it->data, FALSE);
+      med_widget_set_modified (it->data, FALSE);
+      med_widget_set_updated (it->data, TRUE);
       gtk_widget_set_sensitive (((GtkWidget*) it->data), TRUE);
     }
   }
@@ -515,6 +519,7 @@ main_window_process_exec_emu_ended (MedProcess* sender,
     gtk_widget_show ((GtkWidget*) mw);
 
   gtk_widget_set_sensitive (((GtkWidget*) priv->launch_button), TRUE);
+
   med_list_joy_enable_all(priv->listjoy, TRUE);
 
   med_process_read_conf (priv->med_process);
@@ -553,6 +558,47 @@ main_window_bios_entry_emit_change (MedDialogEntry* sender,
 
 
 static void
+main_window_medwid_map (GtkWidget* sender, gpointer self)
+{
+  MainWindowPrivate* priv = main_window_get_instance_private (self);
+  const gchar* command;
+  const gchar* tmp = NULL;
+
+  command = med_widget_get_command ((MedWidget*) sender);
+
+  if (command)
+  {
+    command++;
+    tmp = g_hash_table_lookup (priv->med_process->table, command);
+  }
+
+  if (tmp != NULL)
+  {
+    priv->map_list = g_slist_prepend (priv->map_list, sender);
+
+    if (med_widget_get_updated ((MedWidget*)sender) == FALSE)
+    {
+      med_widget_set_value ((MedWidget*) sender, tmp);
+      med_widget_set_modified ((MedWidget*) sender, FALSE);
+      med_widget_set_updated ((MedWidget*) sender, TRUE);
+      gtk_widget_set_sensitive (sender, TRUE);
+    }
+  }
+  else
+    gtk_widget_set_sensitive (sender, FALSE);
+}
+
+
+static void
+main_window_medwid_unmap (GtkWidget* sender, gpointer self)
+{
+  MainWindowPrivate* priv = main_window_get_instance_private (self);
+
+  priv->map_list = g_slist_remove (priv->map_list, sender);
+}
+
+
+static void
 main_window_get_widgets (MainWindow* self,
                          GtkWidget* wid)
 {
@@ -569,6 +615,9 @@ main_window_get_widgets (MainWindow* self,
     if (IS_MED_WIDGET(it->data))
     {
       priv->list = g_slist_append (priv->list, it->data);
+
+      g_signal_connect_object ((GtkWidget*) it->data, "map", (GCallback) main_window_medwid_map, self, 0);
+      g_signal_connect_object ((GtkWidget*) it->data, "unmap", (GCallback) main_window_medwid_unmap, self, 0);
 
       if (IS_MED_BIOS_ENTRY(it->data))
         g_signal_connect_object ((MedDialogEntry*) it->data, "emit-change", (GCallback) main_window_bios_entry_emit_change, self, 0);
@@ -674,7 +723,7 @@ main_window_menu_quit (GtkMenuItem* sender,
 static void
 save_list_func (gconstpointer data, gpointer self)
 {
-  if (med_widget_get_updated((MedWidget*)data))
+  if (med_widget_get_modified ((MedWidget*)data))
   {
     const gchar* command = med_widget_get_command ((MedWidget*)data);
     const gchar* value = med_widget_get_value ((MedWidget*)data);
@@ -958,7 +1007,6 @@ main_window_start (MainWindow* self)
   g_object_set_data ((GObject*) self, "listjoy", priv->listjoy);
 
   main_window_get_widgets (self, (GtkWidget*) self);
-  main_window_set_values (self);
   main_window_load_settings (self);
 }
 
@@ -973,6 +1021,7 @@ main_window_finalize (GObject * obj)
   g_object_unref (priv->listjoy);
 
   g_slist_free (priv->list);
+  g_slist_free (priv->map_list);
 
   G_OBJECT_CLASS (main_window_parent_class)->finalize (obj);
 }
@@ -1001,7 +1050,7 @@ main_window_new (GtkApplication* app)
   g_signal_connect_object (priv->path_firmware, "emit-change", (GCallback) main_window_firmware_entry_emit_change, self, 0);
 
   med_widget_set_value ((MedWidget*) priv->lynx_bios, "lynxboot.img");
-  med_widget_set_updated ((MedWidget*) priv->lynx_bios, FALSE);
+  med_widget_set_modified ((MedWidget*) priv->lynx_bios, FALSE);
   gtk_widget_set_visible ((GtkWidget*) priv->lynx_bios, FALSE);
 
   priv->pathbox = path_combo_box_new();
@@ -1043,6 +1092,7 @@ main_window_init (MainWindow * self)
   MainWindowPrivate* priv = main_window_get_instance_private (self);
 
   priv->list = NULL;
+  priv->map_list = NULL;
 
   gtk_widget_init_template (GTK_WIDGET (self));
 }

share/glade/catalog/medcatalog.xml
@@ -9,11 +9,6 @@
               <type>GParamString</type>
             </parameter-spec>
         </property>
-        <property id="updated" name="Updated">
-            <parameter-spec>
-              <type>GParamBoolean</type>
-            </parameter-spec>
-        </property>
       </properties>
     </glade-widget-class>
 
@@ -22,11 +17,6 @@
         <property id="command" name="Command">
             <parameter-spec>
               <type>GParamString</type>
-            </parameter-spec>
-        </property>
-        <property id="updated" name="Updated">
-            <parameter-spec>
-              <type>GParamBoolean</type>
             </parameter-spec>
         </property>
         <property id="label" name="Label">
@@ -53,11 +43,6 @@
         <property id="command" name="Command">
             <parameter-spec>
               <type>GParamString</type>
-            </parameter-spec>
-        </property>
-        <property id="updated" name="Updated">
-            <parameter-spec>
-              <type>GParamBoolean</type>
             </parameter-spec>
         </property>
         <property id="label" name="Label">
@@ -91,11 +76,6 @@
               <type>GParamString</type>
             </parameter-spec>
         </property>
-        <property id="updated" name="Updated">
-            <parameter-spec>
-              <type>GParamBoolean</type>
-            </parameter-spec>
-        </property>
         <property id="label" name="Label">
             <parameter-spec>
               <type>GParamString</type>
@@ -116,11 +96,6 @@
               <type>GParamString</type>
             </parameter-spec>
         </property>
-        <property id="updated" name="Updated">
-            <parameter-spec>
-              <type>GParamBoolean</type>
-            </parameter-spec>
-        </property>
         <property id="label" name="Label">
             <parameter-spec>
               <type>GParamString</type>
@@ -139,11 +114,6 @@
         <property id="command" name="Command">
             <parameter-spec>
               <type>GParamString</type>
-            </parameter-spec>
-        </property>
-        <property id="updated" name="Updated">
-            <parameter-spec>
-              <type>GParamBoolean</type>
             </parameter-spec>
         </property>
         <property id="label" name="Label">
@@ -171,11 +141,6 @@
               <type>GParamString</type>
             </parameter-spec>
         </property>
-        <property id="updated" name="Updated">
-            <parameter-spec>
-              <type>GParamBoolean</type>
-            </parameter-spec>
-        </property>
         <property id="label" name="Label">
             <parameter-spec>
               <type>GParamString</type>
@@ -194,11 +159,6 @@
         <property id="command" name="Command">
             <parameter-spec>
               <type>GParamString</type>
-            </parameter-spec>
-        </property>
-        <property id="updated" name="Updated">
-            <parameter-spec>
-              <type>GParamBoolean</type>
             </parameter-spec>
         </property>
         <property id="label" name="Label">

src/widgets/medcheckbutton.c
@@ -35,13 +35,13 @@ struct _MedCheckButtonClass {
 struct _MedCheckButtonPrivate {
   gchar* _command;
   gboolean _updated;
+  gboolean _modified;
 };
 
 
 enum  {
   MED_CHECK_BUTTON_0_PROPERTY,
   MED_CHECK_BUTTON_COMMAND_PROPERTY,
-  MED_CHECK_BUTTON_UPDATED_PROPERTY,
   MED_CHECK_BUTTON_NUM_PROPERTIES
 };
 
@@ -83,7 +83,7 @@ med_check_button_toggled (GtkToggleButton* sender,
                           gpointer self)
 {
   g_return_if_fail (self != NULL);
-  med_widget_set_updated ((MedWidget*) self, TRUE);
+  med_widget_set_modified ((MedWidget*) self, TRUE);
 }
 
 
@@ -131,11 +131,27 @@ med_check_button_real_set_updated (MedWidget* base,
   MedCheckButton* self = (MedCheckButton*) base;
   MedCheckButtonPrivate* priv = med_check_button_get_instance_private(self);
 
-  if (med_check_button_real_get_updated (base) != value)
-  {
-    priv->_updated = value;
-    g_object_notify_by_pspec ((GObject *) self, med_check_button_properties[MED_CHECK_BUTTON_UPDATED_PROPERTY]);
-  }
+  priv->_updated = value;
+}
+
+static gboolean
+med_check_button_real_get_modified (MedWidget* base)
+{
+  MedCheckButton* self = (MedCheckButton*) base;
+  MedCheckButtonPrivate* priv = med_check_button_get_instance_private(self);
+
+  return priv->_modified;
+}
+
+
+static void
+med_check_button_real_set_modified (MedWidget* base,
+                                   gboolean value)
+{
+  MedCheckButton* self = (MedCheckButton*) base;
+  MedCheckButtonPrivate* priv = med_check_button_get_instance_private(self);
+
+  priv->_modified = value;
 }
 
 
@@ -197,9 +213,6 @@ med_check_button_get_property (GObject * object,
     case MED_CHECK_BUTTON_COMMAND_PROPERTY:
       g_value_set_string (value, med_widget_get_command ((MedWidget*) self));
     break;
-    case MED_CHECK_BUTTON_UPDATED_PROPERTY:
-      g_value_set_boolean (value, med_widget_get_updated ((MedWidget*) self));
-    break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
     break;
@@ -219,9 +232,6 @@ med_check_button_set_property (GObject * object,
   {
     case MED_CHECK_BUTTON_COMMAND_PROPERTY:
       med_widget_set_command ((MedWidget*) self, g_value_get_string (value));
-    break;
-    case MED_CHECK_BUTTON_UPDATED_PROPERTY:
-      med_widget_set_updated ((MedWidget*) self, g_value_get_boolean (value));
     break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
@@ -248,17 +258,6 @@ med_check_button_class_init (MedCheckButtonClass * klass)
                                      NULL,
                                      G_PARAM_STATIC_STRINGS | G_PARAM_READABLE | G_PARAM_WRITABLE
                                    ));
-
-  g_object_class_install_property (G_OBJECT_CLASS (klass),
-                                   MED_CHECK_BUTTON_UPDATED_PROPERTY,
-                                   med_check_button_properties[MED_CHECK_BUTTON_UPDATED_PROPERTY] = g_param_spec_boolean
-                                   (
-                                     "updated",
-                                     "updated",
-                                     "updated",
-                                     FALSE,
-                                     G_PARAM_STATIC_STRINGS | G_PARAM_READABLE | G_PARAM_WRITABLE
-                                   ));
 }
 
 
@@ -268,8 +267,12 @@ med_check_button_med_widget_interface_init (MedWidgetInterface * iface)
   iface->set_value = (void (*) (MedWidget *, const gchar*)) med_check_button_real_set_value;
   iface->get_value = (const gchar* (*) (MedWidget *)) med_check_button_real_get_value;
 
+  iface->set_modified = (void (*) (MedWidget *, gboolean)) med_check_button_real_set_modified;
+  iface->get_modified = (gboolean (*) (MedWidget *)) med_check_button_real_get_modified;
+
+  iface->set_updated  = (void (*) (MedWidget *, gboolean)) med_check_button_real_set_updated;
+  iface->get_updated  = (gboolean (*) (MedWidget *)) med_check_button_real_get_updated;
+
   iface->get_command = med_check_button_real_get_command;
   iface->set_command = med_check_button_real_set_command;
-  iface->get_updated = med_check_button_real_get_updated;
-  iface->set_updated = med_check_button_real_set_updated;
 }

src/widgets/medcombobox.c
@@ -37,6 +37,7 @@ struct _MedComboBoxPrivate {
   GtkLabel* combo_label;
   gchar* _command;
   gboolean _updated;
+  gboolean _modified;
   gchar* _label;
   gchar** _values;
   gchar* value;
@@ -46,7 +47,6 @@ struct _MedComboBoxPrivate {
 enum  {
   MED_COMBO_BOX_0_PROPERTY,
   MED_COMBO_BOX_COMMAND_PROPERTY,
-  MED_COMBO_BOX_UPDATED_PROPERTY,
   MED_COMBO_BOX_LABEL_PROPERTY,
   MED_COMBO_BOX_LABELWIDTH_PROPERTY,
   MED_COMBO_BOX_VALUES_PROPERTY,
@@ -161,12 +161,30 @@ med_combo_box_real_set_updated (MedWidget* base,
   MedComboBox* self = (MedComboBox*) base;
   MedComboBoxPrivate* priv = med_combo_box_get_instance_private (self);
 
-  if (med_combo_box_real_get_updated (base) != value)
-  {
-    priv->_updated = value;
-    g_object_notify_by_pspec ((GObject *) self, med_combo_box_properties[MED_COMBO_BOX_UPDATED_PROPERTY]);
-  }
+  priv->_updated = value;
 }
+
+
+static gboolean
+med_combo_box_real_get_modified (MedWidget* base)
+{
+  MedComboBox* self = (MedComboBox*) base;
+  MedComboBoxPrivate* priv = med_combo_box_get_instance_private(self);
+
+  return priv->_modified;
+}
+
+
+static void
+med_combo_box_real_set_modified (MedWidget* base,
+                                   gboolean value)
+{
+  MedComboBox* self = (MedComboBox*) base;
+  MedComboBoxPrivate* priv = med_combo_box_get_instance_private(self);
+
+  priv->_modified = value;
+}
+
 
 static const gchar*
 med_combo_box_get_label (MedComboBox* self)
@@ -176,6 +194,7 @@ med_combo_box_get_label (MedComboBox* self)
 
   return priv->_label;
 }
+
 
 static void
 med_combo_box_set_label (MedComboBox* self,
@@ -239,7 +258,8 @@ med_combo_box_changed (GtkComboBox* sender,
 
   gchar* text = gtk_combo_box_text_get_active_text (priv->combo);
 
-  med_widget_set_updated ((MedWidget*) self, TRUE);
+  med_widget_set_modified ((MedWidget*) self, TRUE);
+
   g_signal_emit (self,
                  med_combo_box_signals[MED_COMBO_BOX_CHANGED_SIGNAL],
                  0,
@@ -343,9 +363,6 @@ med_combo_box_get_property (GObject * object,
     case MED_COMBO_BOX_COMMAND_PROPERTY:
       g_value_set_string (value, med_widget_get_command ((MedWidget*) self));
     break;
-    case MED_COMBO_BOX_UPDATED_PROPERTY:
-      g_value_set_boolean (value, med_widget_get_updated ((MedWidget*) self));
-    break;
     case MED_COMBO_BOX_LABEL_PROPERTY:
       g_value_set_string (value, med_combo_box_get_label (self));
     break;
@@ -371,9 +388,6 @@ med_combo_box_set_property (GObject * object,
   {
     case MED_COMBO_BOX_COMMAND_PROPERTY:
       med_widget_set_command ((MedWidget*) self, g_value_get_string (value));
-    break;
-    case MED_COMBO_BOX_UPDATED_PROPERTY:
-      med_widget_set_updated ((MedWidget*) self, g_value_get_boolean (value));
     break;
     case MED_COMBO_BOX_LABEL_PROPERTY:
       med_combo_box_set_label (self, g_value_get_string (value));
@@ -407,17 +421,6 @@ med_combo_box_class_init (MedComboBoxClass * klass)
                                      "command",
                                      "command",
                                      NULL,
-                                     G_PARAM_STATIC_STRINGS | G_PARAM_READABLE | G_PARAM_WRITABLE
-                                   ));
-
-  g_object_class_install_property (G_OBJECT_CLASS (klass),
-                                   MED_COMBO_BOX_UPDATED_PROPERTY,
-                                   med_combo_box_properties[MED_COMBO_BOX_UPDATED_PROPERTY] = g_param_spec_boolean
-                                   (
-                                     "updated",
-                                     "updated",
-                                     "updated",
-                                     FALSE,
                                      G_PARAM_STATIC_STRINGS | G_PARAM_READABLE | G_PARAM_WRITABLE
                                    ));
 
@@ -473,8 +476,12 @@ med_combo_box_med_widget_interface_init (MedWidgetInterface * iface)
   iface->set_value = (void (*) (MedWidget *, const gchar*)) med_combo_box_real_set_value;
   iface->get_value = (const gchar* (*) (MedWidget *)) med_combo_box_real_get_value;
 
+  iface->set_modified = (void (*) (MedWidget *, gboolean)) med_combo_box_real_set_modified;
+  iface->get_modified = (gboolean (*) (MedWidget *)) med_combo_box_real_get_modified;
+
+  iface->set_updated = (void (*) (MedWidget *, gboolean)) med_combo_box_real_set_updated;
+  iface->get_updated = (gboolean (*) (MedWidget *)) med_combo_box_real_get_updated;
+
   iface->get_command = med_combo_box_real_get_command;
   iface->set_command = med_combo_box_real_set_command;
-  iface->get_updated = med_combo_box_real_get_updated;
-  iface->set_updated = med_combo_box_real_set_updated;
 }

src/widgets/medentry.c
@@ -31,6 +31,7 @@ struct _MedEntryPrivate {
   GtkLabel* entry_label;
   gchar* _command;
   gboolean _updated;
+  gboolean _modified;
   gchar* _label;
 };
 
@@ -38,7 +39,6 @@ struct _MedEntryPrivate {
 enum  {
   MED_ENTRY_0_PROPERTY,
   MED_ENTRY_COMMAND_PROPERTY,
-  MED_ENTRY_UPDATED_PROPERTY,
   MED_ENTRY_LABEL_PROPERTY,
   MED_ENTRY_LABELWIDTH_PROPERTY,
   MED_ENTRY_NUM_PROPERTIES
@@ -77,7 +77,7 @@ med_entry_real_get_value (MedWidget* base)
 static void
 med_entry_real_entry_changed (MedEntry* self)
 {
-  med_widget_set_updated ((MedWidget*) self, TRUE);
+  med_widget_set_modified ((MedWidget*) self, TRUE);
 }
 
 
@@ -126,11 +126,28 @@ med_entry_real_set_updated (MedWidget* base,
   MedEntry* self = (MedEntry*) base;
   MedEntryPrivate* priv = med_entry_get_instance_private (self);
 
-  if (med_entry_real_get_updated (base) != value)
-  {
-    priv->_updated = value;
-    g_object_notify_by_pspec ((GObject *) self, med_entry_properties[MED_ENTRY_UPDATED_PROPERTY]);
-  }
+  priv->_updated = value;
+}
+
+
+static gboolean
+med_entry_real_get_modified (MedWidget* base)
+{
+  MedEntry* self = (MedEntry*) base;
+  MedEntryPrivate* priv = med_entry_get_instance_private (self);
+
+  return priv->_modified;
+}
+
+
+static void
+med_entry_real_set_modified (MedWidget* base,
+                            gboolean value)
+{
+  MedEntry* self = (MedEntry*) base;
+  MedEntryPrivate* priv = med_entry_get_instance_private (self);
+
+  priv->_modified = value;
 }
 
 
@@ -265,9 +282,6 @@ med_entry_get_property (GObject * object,
     case MED_ENTRY_COMMAND_PROPERTY:
       g_value_set_string (value, med_widget_get_command ((MedWidget*) self));
     break;
-    case MED_ENTRY_UPDATED_PROPERTY:
-      g_value_set_boolean (value, med_widget_get_updated ((MedWidget*) self));
-    break;
     case MED_ENTRY_LABEL_PROPERTY:
       g_value_set_string (value, med_entry_get_label (self));
     break;
@@ -290,9 +304,6 @@ med_entry_set_property (GObject * object,
   {
     case MED_ENTRY_COMMAND_PROPERTY:
       med_widget_set_command ((MedWidget*) self, g_value_get_string (value));
-    break;
-    case MED_ENTRY_UPDATED_PROPERTY:
-      med_widget_set_updated ((MedWidget*) self, g_value_get_boolean (value));
     break;
     case MED_ENTRY_LABEL_PROPERTY:
       med_entry_set_label (self, g_value_get_string (value));
@@ -329,17 +340,6 @@ med_entry_class_init (MedEntryClass * klass)
                                    ));
 
   g_object_class_install_property (G_OBJECT_CLASS (klass),
-                                   MED_ENTRY_UPDATED_PROPERTY,
-                                   med_entry_properties[MED_ENTRY_UPDATED_PROPERTY] = g_param_spec_boolean
-                                   (
-                                     "updated",
-                                     "updated",
-                                     "updated",
-                                     FALSE,
-                                     G_PARAM_STATIC_STRINGS | G_PARAM_READABLE | G_PARAM_WRITABLE
-                                   ));
-
-  g_object_class_install_property (G_OBJECT_CLASS (klass),
                                    MED_ENTRY_LABEL_PROPERTY,
                                    med_entry_properties[MED_ENTRY_LABEL_PROPERTY] = g_param_spec_string
                                    (
@@ -368,8 +368,12 @@ med_entry_med_widget_interface_init (MedWidgetInterface * iface)
   iface->set_value = (void (*) (MedWidget *, const gchar*)) med_entry_real_set_value;
   iface->get_value = (const gchar* (*) (MedWidget *)) med_entry_real_get_value;
 
+  iface->set_modified = (void (*) (MedWidget *, gboolean)) med_entry_real_set_modified;
+  iface->get_modified = (gboolean (*) (MedWidget *)) med_entry_real_get_modified;
+
+  iface->set_updated = (void (*) (MedWidget *, gboolean)) med_entry_real_set_updated;
+  iface->get_updated = (gboolean (*) (MedWidget *)) med_entry_real_get_updated;
+
   iface->get_command = med_entry_real_get_command;
   iface->set_command = med_entry_real_set_command;
-  iface->get_updated = med_entry_real_get_updated;
-  iface->set_updated = med_entry_real_set_updated;
 }

src/widgets/medinput.c
@@ -41,6 +41,7 @@ struct _MedInputPrivate {
   MenuInput* menu;
   gchar* _command;
   gboolean _updated;
+  gboolean _modified;
   gchar* _label;
   gboolean _modifier_keys;
   //GtkCssProvider* css_normal;
@@ -58,7 +59,6 @@ struct _MedInputPrivate {
 enum  {
   MED_INPUT_0_PROPERTY,
   MED_INPUT_COMMAND_PROPERTY,
-  MED_INPUT_UPDATED_PROPERTY,
   MED_INPUT_LABEL_PROPERTY,
   MED_INPUT_LABELWIDTH_PROPERTY,
   MED_INPUT_MODIFIER_KEYS_PROPERTY,
@@ -1340,7 +1340,7 @@ med_input_real_set_value (MedWidget* base,
   g_free (text);
   g_free (p);
 
-  med_widget_set_updated (base, TRUE);
+  med_widget_set_modified (base, TRUE);
 }
 
 
@@ -1410,7 +1410,7 @@ input_set_text (MedInput* mi, const gchar* text, const gchar* value)
 
     gtk_toggle_button_set_active ((GtkToggleButton *) priv->entry, FALSE);
     med_input_close_input (mi);
-    med_widget_set_updated ((MedWidget*) mi, TRUE);
+    med_widget_set_modified ((MedWidget*) mi, TRUE);
   }
 }
 
@@ -1632,11 +1632,28 @@ med_input_real_set_updated (MedWidget* base,
   MedInput* self = (MedInput*) base;
   MedInputPrivate* priv = med_input_get_instance_private (self);
 
-  if (med_input_real_get_updated (base) != value)
-  {
-    priv->_updated = value;
-    g_object_notify_by_pspec ((GObject *) self, med_input_properties[MED_INPUT_UPDATED_PROPERTY]);
-  }
+  priv->_updated = value;
+}
+
+
+static gboolean
+med_input_real_get_modified (MedWidget* base)
+{
+  MedInput* self = (MedInput*) base;
+  MedInputPrivate* priv = med_input_get_instance_private (self);
+
+  return priv->_modified;
+}
+
+
+static void
+med_input_real_set_modified (MedWidget* base,
+                            gboolean value)
+{
+  MedInput* self = (MedInput*) base;
+  MedInputPrivate* priv = med_input_get_instance_private (self);
+
+  priv->_modified = value;
 }
 
 
@@ -1748,7 +1765,7 @@ med_input_menu_event (GtkMenuItem* sender,
     g_free (priv->internal_value);
     priv->internal_value = g_strdup ("");
 
-    med_widget_set_updated ((MedWidget*) mi, TRUE);
+    med_widget_set_modified ((MedWidget*) mi, TRUE);
 
     return;
   }
@@ -1914,9 +1931,6 @@ med_input_get_property (GObject * object,
     case MED_INPUT_COMMAND_PROPERTY:
       g_value_set_string (value, med_widget_get_command ((MedWidget*) self));
     break;
-    case MED_INPUT_UPDATED_PROPERTY:
-      g_value_set_boolean (value, med_widget_get_updated ((MedWidget*) self));
-    break;
     case MED_INPUT_LABEL_PROPERTY:
       g_value_set_string (value, med_input_get_label (self));
     break;
@@ -1942,9 +1956,6 @@ med_input_set_property (GObject * object,
 {
     case MED_INPUT_COMMAND_PROPERTY:
       med_widget_set_command ((MedWidget*) self, g_value_get_string (value));
-    break;
-    case MED_INPUT_UPDATED_PROPERTY:
-      med_widget_set_updated ((MedWidget*) self, g_value_get_boolean (value));
     break;
     case MED_INPUT_LABEL_PROPERTY:
       med_input_set_label (self, g_value_get_string (value));
@@ -1978,17 +1989,6 @@ med_input_class_init (MedInputClass * klass)
                                      "command",
                                      "command",
                                      NULL,
-                                     G_PARAM_STATIC_STRINGS | G_PARAM_READABLE | G_PARAM_WRITABLE
-                                   ));
-
-  g_object_class_install_property (G_OBJECT_CLASS (klass),
-                                   MED_INPUT_UPDATED_PROPERTY,
-                                   med_input_properties[MED_INPUT_UPDATED_PROPERTY] = g_param_spec_boolean
-                                   (
-                                     "updated",
-                                     "updated",
-                                     "updated",
-                                     FALSE,
                                      G_PARAM_STATIC_STRINGS | G_PARAM_READABLE | G_PARAM_WRITABLE
                                    ));
 
@@ -2033,8 +2033,12 @@ med_input_med_widget_interface_init (MedWidgetInterface * iface)
   iface->set_value = (void (*) (MedWidget *, const gchar*)) med_input_real_set_value;
   iface->get_value = (const gchar* (*) (MedWidget *)) med_input_real_get_value;
 
+  iface->set_modified = (void (*) (MedWidget *, gboolean)) med_input_real_set_modified;
+  iface->get_modified = (gboolean (*) (MedWidget *)) med_input_real_get_modified;
+
+  iface->set_updated = (void (*) (MedWidget *, gboolean)) med_input_real_set_updated;
+  iface->get_updated = (gboolean (*) (MedWidget *)) med_input_real_get_updated;
+
   iface->get_command = med_input_real_get_command;
   iface->set_command = med_input_real_set_command;
-  iface->get_updated = med_input_real_get_updated;
-  iface->set_updated = med_input_real_set_updated;
 }

commit 3f1543cf488129579e4828bd894ca99606f72dd0

Fix bios check
Broken since 6543f137

src/mainwindow.c
@@ -81,6 +81,30 @@ struct _MainWindowPrivate {
 G_DEFINE_TYPE_WITH_PRIVATE (MainWindow, main_window, GTK_TYPE_APPLICATION_WINDOW);
 
 
+static const gchar*
+main_window_get_table_value (GHashTable* table, MedWidget* wid)
+{
+  const gchar* text = "";
+
+  if ( (med_widget_get_modified (wid)) || (!gtk_widget_get_visible ((GtkWidget*) wid)) )
+  {
+    text = med_widget_get_value (wid);
+  }
+  else
+  {
+    const gchar* command = med_widget_get_command (wid);
+
+    if (command)
+    {
+      command++;
+      text = g_hash_table_lookup (table, command);
+    }
+  }
+
+  return text;
+}
+
+
 static void
 main_window_update_bios (MainWindow* self,
                          MedBiosEntry* entry)
@@ -95,8 +119,8 @@ main_window_update_bios (MainWindow* self,
 
   MainWindowPrivate* priv = main_window_get_instance_private (self);
 
-  text = med_widget_get_value ((MedWidget*) entry);
-  firmware= med_widget_get_value ((MedWidget*) priv->path_firmware);
+  text = main_window_get_table_value (priv->med_process->table, (MedWidget*) entry);
+  firmware = main_window_get_table_value (priv->med_process->table, (MedWidget*) priv->path_firmware);
 
 #ifdef G_OS_WIN32
   mh = g_win32_get_package_installation_directory_of_module (NULL);